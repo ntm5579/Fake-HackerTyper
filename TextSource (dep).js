400901b53accb3b8a21b61e7c1e90c4687e7a5cf
Nathan Malicki <43974544+ntm5579@users.noreply.github.com>
2022-06-07 01:25:54 -0400

    Add files via upload

---
+++ TextSource (dep).js
@@ -0,0 +1,7 @@
+//theming
+var fgColor = rgb(54, 230, 0);
+var bgColor = black;
+
+var textsrcs = [];
+textsrcs.push("function testFunction(){ \n\tconsole.run(c:tunnel.exe);\n} \ntestFunction();\n\nEntering Mainframe...\nfunction checkFireWall(){\n\tvar ip=192.168.1.15;\n}");
+textsrcs.push("\nsecond");
